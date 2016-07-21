96d72236fba8f62ea8738c2ce64b210164fff0bb
traburiss <marlycool@qq.com>
2016-07-21 19:18:55 +0800

    添加错误处理

--- app_service/service_helper/code_deal/equipment_list.py
+++ app_service/service_helper/code_deal/equipment_list.py
@@ -2,15 +2,19 @@ import json
 
 from app_service.service_helper.base_deal_with import BaseDealWith
 from app_service.models import KanmusuEquipment
+from json import JSONDecodeError
 
 
 class EquipmentList(BaseDealWith):
     @staticmethod
     def deal_with(params):
-        receive_params = json.loads(params)
+        try:
+            receive_params = json.loads(params)
+        except JSONDecodeError as e:
+            return json.dumps({"error_code": "1", "error_type": "params error"})
         return_list = []
         db_list = KanmusuEquipment.objects.order_by('equipment_id')
-        if 'PAGE_SIZE' in receive_params.keys() and 'PAGE_NUM' in receive_params.keys():
+        if receive_params is dict and 'PAGE_SIZE' in receive_params.keys() and 'PAGE_NUM' in receive_params.keys():
             page_size = receive_params['PAGE_SIZE']
             page_num = receive_params['PAGE_NUM']
             start_count = page_size * (page_num - 1)
